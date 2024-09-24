bad70cc849ed8ab83d80c73b6138c4a69f117e9a
Курилин Никита Андреевич <kurilinna@pochtabank.ru>
2024-09-24 10:44:40 +0700

    hashROute

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import Initial from './pages/Initial';
@@ -16,31 +16,18 @@ if (!root) throw new Error('root not found');
 
 const container = createRoot(root);
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		children: [
-			{
-				path: '/',
-				element: <Initial />,
-			},
-			{
-				path: '/details',
-				element: <Details />,
-			},
-		],
-	},
-	{
-		path: '*',
-		element: <Navigate to="/" />,
-	},
-]);
-
 container.render(
 	<StrictMode>
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<App />}>
+						<Route index element={<Initial />} />
+						<Route path="details" element={<Details />} />
+						<Route path="*" element={<Navigate to="/" />} />
+					</Route>
+				</Routes>
+			</HashRouter>
 		</Provider>
 	</StrictMode>
 );
